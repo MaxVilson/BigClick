526adeeddc15d0ecd2399e860367195d6d5d4dfc
Max <maxim00066@gmail.com>
2020-01-13 04:54:52 +0300

    create datepicker

--- assets/src/js/documentReady.js
+++ assets/src/js/documentReady.js
@@ -1,11 +1,9 @@
-// /* global document module */
+const ready = function (fn) {
+    if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
+        fn();
+    } else {
+        document.addEventListener('DOMContentLoaded', fn);
+    }
+}
 
-// const ready = function (fn) {
-//     if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
-//         fn();
-//     } else {
-//         document.addEventListener('DOMContentLoaded', fn);
-//     }
-// }
-
-// module.exports = ready;
+module.exports = ready;
--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,22 +1,28 @@
-// // Импортируем jQuery
-// import '../../../node_modules/jquery/dist/jquery.js';
+// Для ипорта используется плагин gulp-rigger, для того чтобы импортировать файл, нужно вначале прописать //= (это что-то типо импорт в js =))))
 
-// // Импортируем Popper
-// import '../../../node_modules/popper.js/dist/umd/popper.js';
+// Импортируем jQuery
+//= '../../../node_modules/jquery/dist/jquery.js'
 
-// // Импортируем необходимые js-файлы Bootstrap 4
-// //= ../../../node_modules/bootstrap/js/dist/util.js
-// //= ../../../node_modules/bootstrap/js/dist/alert.js
-// //= ../../../node_modules/bootstrap/js/dist/button.js
-// //= ../../../node_modules/bootstrap/js/dist/carousel.js
-// import '../../../node_modules/bootstrap/js/dist/collapse.js';
-// //= ../../../node_modules/bootstrap/js/dist/dropdown.js
-// //= ../../../node_modules/bootstrap/js/dist/modal.js
-// //= ../../../node_modules/bootstrap/js/dist/tooltip.js
-// //= ../../../node_modules/bootstrap/js/dist/popover.js
-// //= ../../../node_modules/bootstrap/js/dist/scrollspy.js
-// //= ../../../node_modules/bootstrap/js/dist/tab.js
-// //= ../../../node_modules/bootstrap/js/dist/toast.js
+// Импортируем Popper
+//= '../../../node_modules/popper.js/dist/umd/popper.js'
 
-// // Импортируем другие js-файлы
-// //= my.js
+// Импортируем библиотеку airDatePicker
+//= '../../../node_modules/air-datepicker/dist/js/datepicker'
+
+// Импортируем необходимые js-файлы Bootstrap 4
+//= '../../../node_modules/bootstrap/js/dist/util.js'
+//= '../../../node_modules/bootstrap/js/dist/alert.js'
+//= '../../../node_modules/bootstrap/js/dist/button.js'
+//= '../../../node_modules/bootstrap/js/dist/carousel.js'
+//= '../../../node_modules/bootstrap/js/dist/collapse.js'
+//= '../../../node_modules/bootstrap/js/dist/dropdown.js'
+//= '../../../node_modules/bootstrap/js/dist/modal.js'
+//= '../../../node_modules/bootstrap/js/dist/tooltip.js'
+//= '../../../node_modules/bootstrap/js/dist/popover.js'
+//= '../../../node_modules/bootstrap/js/dist/scrollspy.js'
+//= '../../../node_modules/bootstrap/js/dist/tab.js'
+//= '../../../node_modules/bootstrap/js/dist/toast.js'
+
+// Импортируем другие js-файлы
+//= 'my.js'
+//= '../components/date-picker/date-picker.js'
