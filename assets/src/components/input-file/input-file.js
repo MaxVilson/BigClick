0c50f19b1839abcc98bd4e8068756b638e8e522a
Max <maxim00066@gmail.com>
2020-02-08 15:59:25 +0300

    fix jq2

--- assets/src/components/input-file/input-file.js
+++ assets/src/components/input-file/input-file.js
@@ -1,16 +1,18 @@
-function readURL(input) {
+$(document).ready(function () {
+    function readURL(input) {
 
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
 
-        reader.onload = function (e) {
-            $('.input-file__img').attr('src', e.target.result);
-        };
+            reader.onload = function (e) {
+                $('.input-file__img').attr('src', e.target.result);
+            };
 
-        reader.readAsDataURL(input.files[0]);
+            reader.readAsDataURL(input.files[0]);
+        }
     }
-}
 
-$(".input-file__input").change(function () {
-    readURL(this);
-});
+    $(".input-file__input").change(function () {
+        readURL(this);
+    });
+})
